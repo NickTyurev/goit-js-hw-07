85101ae10f4bbd0bfecf24cf94b7f50815804154
Nick_Tyurev <nickverysmart@gmail.com>
2024-10-03 13:04:47 +0300

    jshw7

---
+++ js/task-5.js
@@ -0,0 +1,22 @@
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+
+
+const button = document.querySelector('button.change-color');
+const span = document.querySelector('span.color');
+
+
+button.addEventListener('click', () => {
+ 
+  const randomColor = getRandomHexColor();
+  
+  
+  document.body.style.backgroundColor = randomColor;
+
+  
+  span.textContent = randomColor;
+});
